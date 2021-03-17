b0cbf68dca8c64a0ca8a7240370e5bb9f2ddb6c4
Patricio Jofre <luis.jofre.g@gmail.com>
2021-03-17 19:30:48 -0300

    add json for mysql

--- lib/active_record/connection_adapters/nulldb_adapter/table_definition.rb
+++ lib/active_record/connection_adapters/nulldb_adapter/table_definition.rb
@@ -1,11 +1,14 @@
-class ActiveRecord::ConnectionAdapters::NullDBAdapter
+# frozen_string_literal: true
 
+class ActiveRecord::ConnectionAdapters::NullDBAdapter
   class TableDefinition < ActiveRecord::ConnectionAdapters::TableDefinition
     attr_accessor :name
-    alias_method :uuid, :string
-    alias_method :citext, :text
-    alias_method :interval, :text
-    alias_method :geometry, :text
-    alias_method :jsonb, :json if method_defined? :json
+    alias uuid string
+    alias citext text
+    alias interval text
+    alias geometry text
+    alias jsonb json if method_defined? :json
+
+    def json(args); end
   end
 end
--- lib/nulldb/version.rb
+++ lib/nulldb/version.rb
@@ -1,3 +1,3 @@
 module NullDB
-  VERSION = "0.7.0"
+  VERSION = "0.7.1"
 end
